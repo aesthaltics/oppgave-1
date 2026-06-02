fab5d267459886a3d8861a36c87cacfbd373896c
jakob <aesthaltics@pm.me>
2026-06-02 13:59:42 +0200

    made it better

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const search = document.getElementsByName("destination-search")[0]
+window.addEventListener("load", (event) => {
+  const search = document.getElementsByName("destination-search")[0];
 
-search.addEventListener("input", (event) => {
-    console.log(event.target.value)
-})
+  search.addEventListener("input", (event) => {
+    console.log(event.target.value);
+  });
+  const dashboard = document.querySelector(".dashboard");
+  const heroButton = document.querySelector(".hero-button");
+  heroButton.addEventListener("click", (event) => {
+    dashboard.scrollIntoView({
+      block: "start",
+      inline: "nearest",
+      behavior: "smooth",
+    });
+  });
+});
