c64f93da9e94923324b72e01b8ca909015e7e673
jakob <aesthaltics@pm.me>
2026-06-05 10:44:48 +0200

    added autocomplete with google places(new) api

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 window.addEventListener("load", (event) => {
-  const search = document.getElementsByName("destination-search")[0];
+  const searchElement = document.getElementsByName("destination-search")[0];
+  const search = debounce((query) => init(query), 300);
 
-  search.addEventListener("input", (event) => {
-    console.log(event.target.value);
+  searchElement.addEventListener("input", (event) => {
+    search(event.target.value);
   });
   const dashboard = document.querySelector(".dashboard");
   const heroButton = document.querySelector(".hero-button");
@@ -14,3 +15,65 @@ window.addEventListener("load", (event) => {
     });
   });
 });
+
+const debounce = (callback, ms = 300) => {
+  let t;
+  return (...args) => {
+    clearTimeout(t);
+    t = setTimeout(() => callback(...args), ms);
+  };
+};
+
+async function init(query) {
+  let newAutocompleteContainer = document.createElement("div");
+  newAutocompleteContainer.className = "auto-complete";
+  console.log(query);
+  console.log(query === "");
+  let searchInputContainer = document.querySelector(".search-input-container");
+  if (query === "") {
+    console.log("empty query");
+    for (const child of searchInputContainer.children) {
+      if (child.className === "auto-complete") {
+        child.remove();
+      }
+    }
+    return;
+  }
+
+  const { AutocompleteSessionToken, AutocompleteSuggestion } =
+    await google.maps.importLibrary("places");
+
+  // Add an initial request body.
+  const request = {
+    input: query,
+    includedPrimaryTypes: ["(cities)"],
+  };
+
+  // Create a session token.
+  const token = new AutocompleteSessionToken();
+  // Add the token to the request.
+  request.sessionToken = token;
+  // Fetch autocomplete suggestions.
+  const { suggestions } =
+    await AutocompleteSuggestion.fetchAutocompleteSuggestions(request);
+
+  for (const suggestion of suggestions) {
+    const suggestionDiv = document.createElement("div");
+    const place = suggestion.placePrediction.toPlace();
+    await place.fetchFields({
+      fields: ["formattedAddress"],
+    });
+    suggestionDiv.innerText = `${place.formattedAddress}`;
+    newAutocompleteContainer.appendChild(suggestionDiv);
+    suggestionDiv.className = "auto-complete-suggestion";
+  }
+  console.log(searchInputContainer.children);
+
+  for (const child of searchInputContainer.children) {
+    if (child.className === "auto-complete") {
+      child.remove();
+    }
+  }
+
+  searchInputContainer.appendChild(newAutocompleteContainer);
+}
